f3e7ce50813840989fb97e824542169861e31c73
Marp-Toad <130750660+Marp-Toad@users.noreply.github.com>
2023-04-15 06:27:24 +0500

    Add files via upload

---
+++ Class2/class2.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+
+class Toad
+{
+
+    double a, b, c;
+
+public:
+
+    Toad()
+    {
+        a = 0;
+        b = 0;
+        c = 0;
+    }
+
+    Toad(double A, double B, double C)
+    {
+        a = A;
+        b = B;
+        c = C;
+    }
+
+    Toad(const Toad &Frog)
+    {
+        a = Frog.a;
+        b = Frog.b;
+        c = Frog.c;
+    }
+    double getToad()
+    {
+        return a, b, c;
+    }
+  
+    void showAtt()
+    {
+
+        cout << a << "\n";
+        cout << b << "\n";
+        cout << c << "\n";
+
+    }
+
+    ~Toad()
+    {
+
+    }
+};
+
+int main() {
+
+    Toad Frog1;
+    Toad Frog2(1.0, 2.1, 3.21);
+    Toad Frog3 = Frog1;
+
+    Frog2.showAtt();
+}
